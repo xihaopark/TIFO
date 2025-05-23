cc0aca39b19c9fb8d5525ea78f92c139a04fbec7
xihao park <103370372+xihaopark@users.noreply.github.com>
2025-05-23 14:16:35 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# Anonymous Submission Code for ICLR2025-13956
+
+## Introduction
+
+This repository contains the anonymous submission code for **ICLR2025-13956**. 
+To maintain anonymity, the code does not include any identity information.
+
+## Getting Started
+
+### Running the Test Sample
+
+To start the provided test sample, you can run the following command:
+
+```bash
+sh scripts/long_term_forecast/ETT_script/iTransformer_ETTh1.sh
+```
+
+The data used in the test sample is already included in this GitHub repository.
+
+## Source Code
+
+Our source code can be found in the following file:
+
+- **utils/frequency_domain_filter.py**
+
+References to this module can be found in:
+
+- **exp/exp_long_term_forecasting.py**
+- **models/iTransformer.py**
+
+## Pre-trained Models and Logs
+
+You can choose to use our trained sample model directly. 
+It is included in the checkpoints folder and will be automatically called during execution.
+
+Alternatively, you can view our provided sample training logs in the **logs** folder and check the training results in the **result_long_term_forecast.txt** file.
+
+## Issues
+
+If you encounter any issues while executing the files, please mention them in the review comments.
+
+We appreciate the reviewers' voluntary efforts and will do our best to respond carefully to each review comment.
---
+++ utils/frequency_domain_filter.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+torch.autograd.set_detect_anomaly(True)
+class GlobalMaskCalculator:
+    def __init__(self, args, device):
+        self.args = args
+        self.device = device
+
+    def compute_global_statistics(self, train_loader):
+        num_channels = self.args.enc_in
+        freq_length = self.args.seq_len # // 2 + 1
+
+        amplitude_sum = torch.zeros(freq_length, num_channels, dtype=torch.float32).to(self.device)
+        amplitude_squared_sum = torch.zeros(freq_length, num_channels, dtype=torch.float32).to(self.device)
+        count = 0
+
+        with torch.no_grad():
+            for data in train_loader:
+                #print("data:",data.shape)
+                lookback_window = data[0].float().to(self.device)
+                #print("lookback:",lookback_window.shape) #[batch, length, channel]
+                xf = torch.fft.fft(lookback_window, dim=1)
+                amplitude = torch.abs(xf)
+
+                amplitude_sum += amplitude.sum(dim=0)
+                amplitude_squared_sum += (amplitude ** 2).sum(dim=0)
+                count += lookback_window.size(0)
+
+        mean_amplitude = amplitude_sum / count
+        mean_amplitude_squared = amplitude_squared_sum / count
+        variance_amplitude = mean_amplitude_squared - (mean_amplitude ** 2)
+        std_amplitude = torch.sqrt(variance_amplitude + 1e-5)
+
+        global_mask_amp = mean_amplitude / (std_amplitude + 1e-5)
+
+        return global_mask_amp
+
+def run_filter(args, train_loader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    calculator = GlobalMaskCalculator(args, device)
+    global_mask_amp = calculator.compute_global_statistics(train_loader)
+    return global_mask_amp
+
+def apply_difference(data, n=1):
+    """
+    Apply differencing to the data.
+    :param data: Input data [batch, length, channel]
+    :param n: Order of differencing
+    :return: Differenced data and the last original data point for each series
+    """
+    for i in range(n):
+        data[:, 1:, :] = data[:, 1:, :] - data[:, :-1, :]
+    last_value = data[:, -1:, :]
+    return data
+
+class FrequencyDomainFilter(nn.Module):
+    def __init__(self, args, global_mask_amp):
+        super(FrequencyDomainFilter, self).__init__()
+        self.args = args
+        num_channels = args.enc_in
+        self.output_length = args.pred_len
+        self.mask = global_mask_amp
+        hidden = 256
+        # Global filter linear layers with L2 regularization and Dropout
+        self.linear_r = nn.Sequential(
+            nn.Linear(global_mask_amp.size(0), hidden),  # bottleneck structure
+            nn.RReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(hidden, global_mask_amp.size(0)),
+        )
+        
+        self.linear_i = nn.Sequential(
+            nn.Linear(global_mask_amp.size(0), hidden),  # bottleneck structure
+            nn.RReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(hidden, global_mask_amp.size(0)),
+        )
+
+    def apply_global_filter(self, data):
+        data_fft = torch.fft.fft(data, dim=1)
+        mask_r = self.linear_r(self.mask.permute(1, 0)).permute(1, 0)
+        mask_i = self.linear_i(self.mask.permute(1, 0)).permute(1, 0)
+        masked_fft_real = data_fft.real * mask_r
+        masked_fft_imag = data_fft.imag * mask_i
+        masked_fft = torch.complex(masked_fft_real, masked_fft_imag)
+        filtered_data = torch.fft.ifft(masked_fft, dim=1, n=data.size(1)).real
+        return filtered_data
+
+    def forward(self, batch_x):
+        differenced_data = apply_difference(batch_x, n=1)
+        filtered_data = self.apply_global_filter(differenced_data)
+        return filtered_data
